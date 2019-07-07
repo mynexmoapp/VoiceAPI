7519bb7430f31fa166c33d97592dd96ce36bab29
Stephen Wolf <contact@neoswit.fr>
2019-07-07 17:30:33 +0200

    3rd commit

--- application.py
+++ application.py
@@ -1,6 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+import nexmo
+client = nexmo.Client(application_id=ea53dec3-c930-4333-a9e5-bb97e9e662d7, private_key=c93c8331)
+
+from pprint import pprint
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+	response = client.create_call({
+	'to': [{'type': 'phone', 'number': '33781639678'}],
+	'from': {'type': 'phone', 'number': '33644633287'},
+	'answer_url': ['https://example.com/answer']
+	})
+    return "Hello! I'm going to call you"
